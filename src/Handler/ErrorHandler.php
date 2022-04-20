c3fabee4878edaaf68bf8891b9e27cb64d6fed55
margud <marius.gudauskis@invertus.eu>
2022-04-20 09:32:04 +0300

    sentry lib handler

--- src/Handler/ErrorHandler.php
+++ src/Handler/ErrorHandler.php
@@ -1,9 +1,11 @@
 <?php
 
-use Configuration;
-use Module;
-
 namespace PrestaShop\Sentry\Handler;
+
+use Configuration;
+use Exception;
+use Module;
+use Sentry\Severity;
 
 /**
  * Handle Error.
@@ -28,16 +30,52 @@ class ErrorHandler implements ErrorHandlerInterface
             );
             $scope->setTags(
                 [
-                    'ps_eventbus_version' => $module->version,
+                    'module_version' => $module->version,
                     'php_version' => phpversion(),
                     'prestashop_version' => _PS_VERSION_,
-                    'ps_eventbus_is_enabled' => (int) Module::isEnabled($module->name),
-                    'ps_eventbus_is_installed' => (int) Module::isInstalled($module->name),
+                    'module_is_enabled' => (int) Module::isEnabled($module->name),
+                    'module_is_installed' => (int) Module::isInstalled($module->name),
                 ]
             );
         });
     }
 
+    /**
+     * @param array<string, string> $tags The tags to merge into the current context
+     */
+    public function setTags(array $tags)
+    {
+        \Sentry\configureScope(function (\Sentry\State\Scope $scope) use ($tags): void {
+            $scope->setTags(
+                $tags
+            );
+        });
+    }
+
+    /**
+     * @param Severity|null $level The severity
+     */
+    public function setLevel(Severity $level)
+    {
+        \Sentry\configureScope(function (\Sentry\State\Scope $scope) use ($level): void {
+            $scope->setLevel($level);
+        }); 
+    }
+    
+    /**
+     * @param array<string, mixed> $data  The data
+     * @param bool $merge If true, $data will be merged into user context instead of replacing it
+     */
+    public function setUser(array $data, bool $merge = false)
+    {
+        \Sentry\configureScope(function (\Sentry\State\Scope $scope) use ($data, $merge): void {
+            $scope->setUser(
+                $data,
+                $merge
+            );
+        });
+    }
+    
     /**
      * @param Exception $error
      * @param mixed $code
